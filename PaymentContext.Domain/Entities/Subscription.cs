ba1a7f8763d48754603c3a2aa007b0039d099ade
RafaelBortolozoLuxSistemas <rafaelbortolozo@luxsistemas.com.br>
2023-01-12 17:53:31 -0300

    Tratamento de acesso a propriedades

---
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -0,0 +1,20 @@
+﻿namespace PaymentContext.Domain.Entities
+{
+    public class Subscription
+    {
+        public Subscription(bool active, DateTime createDate, DateTime lastUpdateDate, DateTime? expireDate, List<Payment> payments)
+        {
+            Active = active;
+            CreateDate = DateTime.Now;
+            LastUpdateDate = lastUpdateDate;
+            ExpireDate = expireDate;
+            Payments = payments;
+        }
+
+        public bool Active { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public DateTime? ExpireDate { get; set; }
+        public List<Payment> Payments { get; set; }
+    }
+}
---
+++ PaymentContext.Tests/Entities/StudentTest.cs
@@ -0,0 +1,16 @@
+﻿using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTest
+    {
+        [TestMethod]
+        public void StudentTest1()
+        {
+            var subscription = new Subscription();
+            var student = new Student("Bruno", "Potrikus", "1234567890", "bruno@email.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
